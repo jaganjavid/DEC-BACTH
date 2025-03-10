6fed6433aa5128768291a64e7ec9f3a031cb7200
Jagan <110019353+jaganjavid@users.noreply.github.com>
2025-03-10 21:02:35 +0530

    Added a backend post request

--- 4-mern/e-com/backend/app.js
+++ 4-mern/e-com/backend/app.js
@@ -4,12 +4,19 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import { connectDatabase } from "./config/dbConnect.js";
+
+
 import productRoutes from "./routes/products.js"
 
 const app  = express();
 
 dotenv.config( { path:"backend/config/config.env" } );
 
+connectDatabase();
+
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({
